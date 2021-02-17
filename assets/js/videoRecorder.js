a5f17d2144592efbb591e99b2840c3bc3cbf8010
jihun04 <yujihun20060217@gmail.com>
2021-02-17 16:45:34 +0900

    Recording Video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,37 @@ const recordContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn"),
   videoPreview = document.getElementById("jsVideoPreview");
 
-async function handleRecordBtnClick() {
+let streamObject,
+  videoRecoreder;
+
+function handleVideoData(event) {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+}
+
+function startRecording() {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+}
+
+function stopRecording() {
+  videoPreview.style.height = "0";
+  streamObject.getTracks().forEach(function (track) {
+    track.stop();
+  });
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerText = "Start recording";
+}
+
+async function getVideo() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -11,16 +41,19 @@ async function handleRecordBtnClick() {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
-    videoPreview.style.height = "210px";
-    console.log(stream);
+    videoPreview.style.height = "209px";
+    recordBtn.innerText = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerText = "Can't record :(";
-    recordBtn.removeEventListener("click", handleRecordBtnClick);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 }
 
 function init() {
-  recordBtn.addEventListener("click", handleRecordBtnClick);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recordContainer) {
