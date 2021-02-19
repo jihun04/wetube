0acffe82e891c3908d726794d0f6c34966e05095
jihun04 <yujihun20060217@gmail.com>
2021-02-19 11:45:04 +0900

    API Adding a Comment part Three

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-const addCommentForm = document.getElementById("jsAddComment");
+const addCommentForm = document.getElementById("jsAddComment"),
+  commentList = document.querySelector(".video__comments-list"),
+  commentNumber = document.querySelector(".video__comment-number");
 
 function addComment(response, comment) {
-  const commentList = document.querySelector(".video__comments-list");
   const li = document.createElement("li");
   const avatar = document.createElement("div");
   const textBox = document.createElement("div");
   const username = document.createElement("span");
   const date = document.createElement("span");
   const text = document.createElement("p");
-  if (response.data.avatarUrl.includes("http")) {
+  const commentInnerNumber = parseInt(commentNumber.innerText.split("comment")[0]);
+  if (response.data.avatarUrl.substring(0, 4) === "http") {
     avatar.style.backgroundImage = `url('${response.data.avatarUrl}')`;
   } else {
     avatar.style.backgroundImage = `url('/${response.data.avatarUrl}')`;
@@ -18,6 +20,7 @@ function addComment(response, comment) {
   username.innerText = response.data.name;
   date.innerText = "just a moment ago";
   text.innerText = comment;
+  commentNumber.innerText = `${commentInnerNumber > 0 ? `${commentInnerNumber} comments` : `${commentInnerNumber} comment`}`;
   avatar.classList.add("comment__u-avatar");
   textBox.classList.add("comment__text");
   username.classList.add("comment__username");
@@ -32,14 +35,15 @@ function addComment(response, comment) {
 async function sendComment(comment) {
   const id = window.location.href.split("/videos/")[1];
   const response = await axios({
-    url: `/api/${id}/comment`,
+    url: `/ api / ${id} /comment`,
     method: "POST",
     data: {
       comment
     }
   });
-  console.log(response);
-  addComment(response, comment);
+  if (response.status === 200) {
+    addComment(response, comment);
+  }
 }
 
 function handleSubmit(event) {
