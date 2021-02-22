20b3e56147efdf6594020f54ea22d3db08a282f7
jihun04 <yujihun20060217@gmail.com>
2021-02-22 21:01:56 +0900

    User Detail Navigation Bar part Two

--- assets/js/userDetailNavigation.js
+++ assets/js/userDetailNavigation.js
@@ -1,7 +1,19 @@
 const navigationBar = document.getElementById("jsNavigationBar"),
   navigationList = document.getElementById("jsNavigationList"),
   previousBtn = document.getElementById("jsPreviousBtn"),
-  nextBtn = document.getElementById("jsNextBtn");
+  nextBtn = document.getElementById("jsNextBtn"),
+  navigationItems = document.querySelectorAll("#jsNavigationList li"),
+  selectedBar = document.getElementById("jsNavigationSelectedBar");
+
+const widthList = [
+  115.33,
+  124.46,
+  147.7,
+  153.46,
+  164.99,
+  122.09,
+  128.49
+]
 
 function loadBtns() {
   const listWidth = navigationList.scrollWidth;
@@ -41,11 +53,25 @@ function handleNextBtnClick() {
   });
 }
 
+function handleNvigationItemClick() {
+  const target = this;
+  const oldItem = navigationList.querySelector(".navigation-item--selected");
+  const translateX = target.offsetLeft;
+  selectedBar.style.transform = `translateX(${translateX}px)`;
+  selectedBar.style.width = `${target.scrollWidth}px`;
+  target.classList.add("navigation-item--selected");
+  oldItem.classList.remove("navigation-item--selected");
+  window.location.href = window.location.href + `?query=${target.id}`
+}
+
 function init() {
   loadBtns();
   navigationList.addEventListener("scroll", handleListScroll);
   previousBtn.addEventListener("click", handlePreviousBtnClick);
   nextBtn.addEventListener("click", handleNextBtnClick);
+  for (const item of navigationItems) {
+    item.addEventListener("click", handleNvigationItemClick);
+  }
 }
 
 if (navigationBar) {
