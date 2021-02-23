88251c067eae92286a190a21b7b190d45a525dce
jihun04 <yujihun20060217@gmail.com>
2021-02-23 22:00:21 +0900

    Update

--- src/init.js
+++ src/init.js
@@ -14,4 +14,4 @@ const PORT = process.env.PORT || 4010;
 const handleListening = () =>
   console.log(`✅  Listening on: http://localhost:${PORT}`);
 
-app.listen(PORT, '0.0.0.0', handleListening);
+app.listen(PORT, handleListening);
